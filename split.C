8de0d0ab04c5d0e86147c9dd1ab8c0ad4f790d34
JAY PONDA <77849303+JayPonda@users.noreply.github.com>
2022-10-14 17:29:42 +0530

    Update split.C
    multi list support, add getWordAt() function

--- split.C
+++ split.C
@@ -13,33 +13,41 @@ struct Word
 
 struct Word *head;
 
-void insertWord(char* c){
+struct List
+{
+    struct Word *head;
+    struct Word *poll;
+};
+
+void insertWord(List* list,char* c){
     struct Word *WordArray;
-    WordArray = (struct Word*)malloc(sizeof(struct Word));  
+    WordArray = (struct Word*)malloc(sizeof(struct Word));
     
     if(WordArray == NULL){  
         printf("\nOVERFLOW");  
     }  
     else  {  
     
-    if(head==NULL)  {  
+    if(list->poll==NULL)  {  
         WordArray->nextWord = NULL;  
         WordArray->previousWord=NULL;  
         WordArray->word=c;
         WordArray->wordLen=strlen(c);
         WordArray->wordIndex=0;
-        head=WordArray;
+        list->head=WordArray;
+        list->poll=WordArray;
     }  
     else   {  
         WordArray->word=c;  
-        WordArray->previousWord=head;  
+        WordArray->previousWord=list->head;  
         WordArray->nextWord = NULL;
         WordArray->wordLen=strlen(c);
-        WordArray->wordIndex=head->wordIndex + 1;
-        head->nextWord=WordArray;  
-        head=WordArray;  
-    }  
-    printf("\nWord: <%s> Node inserted\n", WordArray->word);  
+        WordArray->wordIndex=list->head->wordIndex + 1;
+        list->head->nextWord=WordArray;  
+        list->head=WordArray;  
+    } 
+
+      
     }  
 }
 
@@ -56,7 +64,10 @@ char* getSubString1(char str[], int s, int e){
     return newOne;
 }
 
-void split(char str[]){
+List* split(char str[]){
+    struct List *list = (List*)malloc(sizeof(List));
+    list->poll=NULL;
+    list->head=NULL;
     int len = strlen(str);
     int t;
     
@@ -64,6 +75,7 @@ void split(char str[]){
     int sWord = 0;
     int eWord = 0;
     bool isWordStarted = false;
+
     for(t = 0; t < len; t++){
         
         if(str[t] != ' '){
@@ -76,39 +88,94 @@ void split(char str[]){
         else{
             if(isWordStarted){
                 eWord = t;
-                insertWord(getSubString1(str, sWord, eWord - 1));
+                insertWord(list, getSubString1(str, sWord, eWord - 1));
                 isWordStarted = false;
             }
         }
     }
     if(isWordStarted)
-        insertWord(getSubString1(str, sWord, len));
+        insertWord(list, getSubString1(str, sWord, len));
+    
+    return list;
 }
 
-void printWords(){
-    struct Word *temp; 
-    struct Word *temp1;  
-    printf("\nprinting values...\n");  
-    temp = head;
-    temp1 = head;
-    while(temp != NULL)  
-    {  
-        temp1=temp->previousWord;
-        if(temp1 == NULL)
-            break;
-        temp = temp1;  
-    } 
+void printWords(List* list){
     
-    while(temp != NULL)  
-    {  
-        printf("~%s~\t",temp->word);
-        temp=temp->nextWord;  
-    }  
+    struct Word *temp;  
+    printf("\nhere is your words...\n");   
+    temp=list->poll;
+    
+    while(temp != NULL){
+        printf("\n%d: %s", temp->wordIndex,temp->word);
+        temp=temp->nextWord;
+    }
+    printf("\n");
+
+}
+
+char* getWordAt(List* list, int ind){
+
+    int minIndex = list->poll->wordIndex;
+    
+    int maxIndex = list->head->wordIndex;
+
+    int indCpy = ind;
+    Word* word;
+
+    if(ind < 0)
+        ind = maxIndex + ind + 1;
+    
+    if(ind < 0 || ind > maxIndex){
+        printf("\nIndexOutOfBound: index is more then it's item %d", indCpy);
+        return NULL;
+    };
+    
+    // binary search
+
+    if(ind == minIndex)
+        return list->poll->word;
+
+    else if(ind == maxIndex)
+        return list->head->word;
+
+    else{
+        // for desiding where should I starting iteration; 
+        if(ind - minIndex < maxIndex - ind){
+            word = list->poll->nextWord;
+            
+            while (word->wordIndex != ind)
+                word = word->nextWord;
+        }
+        else{
+            word = list->head->previousWord;
+            
+            while (word->wordIndex != ind)
+                word = word->previousWord;
+        }
+            
+        return word->word;
+    };
+  
 }
 
 int main(){
-    char s[] = "this is my sentense";
-    split(s);
-    printWords();
+    char s[] = "this is my sentense    ";
+    char s1[] = "     hello this will be my new list,      !";
+    char s2[] = "Dolore sint quis mollit esse. Dolore occaecat anim excepteur tempor reprehenderit enim qui proident sint ea duis. Aute consequat laborum labore cillum elit velit culpa labore elit labore quis enim aliquip irure. Pariatur voluptate laboris aute mollit qui proident id. Occaecat minim Lorem eiusmod ex Lorem sint cupidatat esse ea duis ex labore sunt. Magna ut occaecat ipsum ullamco reprehenderit nisi labore aliqua. Velit in aliqua labore magna esse do amet qui amet sit incididunt ea anim.";
+    int myInd[] = {-15, -3, 0, 3, 15, 30, -16, -32, 28, -100, 100};
+    
+    struct List* list = split(s);
+    printWords(list);
+
+    struct List* list1 = split(s1);
+    printWords(list1);
+
+    struct List* list2 = split(s2);
+    printWords(list2);
+
+
+    for(int i = 0; i < 11; i++)
+        printf("\n%d: %d %s\n", i + 1, myInd[i] ,getWordAt(list2, myInd[i]));
+    
     return 0;
 }
